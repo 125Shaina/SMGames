2b94cb17aa34288eb0ac95bf6a5230d32c44463d
Shaina Mitra <125smitra@frhsd.com>
2024-12-20 11:41:31 -0500

    s

--- games/src/githubsearch.tsx
+++ games/src/githubsearch.tsx
@@ -9,6 +9,7 @@ export default function Githubsearch()
 function handleSearchButton() {
     <h1 style={{fontFamily: "Courier New"}}>{search}</h1>
 
+
     if (prevSearch !== search) {
         fetch("https://api.github.com/search/users?q=" + search + "%20in:login")
             .then((response) => response.json())
@@ -25,22 +26,24 @@ function handleSearchButton() {
 
     return (
         <>
-            <div style={{justifyContent:"center", margin: "auto"}}>
+            <div>
 
-                API Github
+                <h1 style = {{fontFamily: "Courier New"}}> API Github </h1>
+                <div style={{justifyContent: "center", margin: "auto"}}>
                 <input
                     type="text"
-                    style={{ backgroundColor: 'lightblue', border: '1px round gray' }}
+                    style={{ textAlign:"center",justifyContent:"center", backgroundColor: 'lightblue', border: '1px round gray' }}
                     onChange={(e) => setSearch(e.target.value)}
                     value={search}
                     placeholder = "Search for GitHub Users"
                     />
+                </div>
                 <button onClick={handleSearchButton}>Search</button>
 
 
             </div>
             { // @ts-ignore
-                results.map(item => <div><img style = {{height: "100px", width: "100px", borderRadius: "3rem", padding:"5px", width:"100px",margin:"auto"}} src = {item.avatar_url}/><div style={{fontFamily: "Courier New", display:"flex"}}> {item.login}</div></div>)
+                results.map(item => <div style ={{textAlign:"center"}}><img style = {{justifyContent:"center",height: "100px", width: "100px", borderRadius: "3rem", padding:"5px", width:"100px",margin:"auto"}} src = {item.avatar_url}/><div style={{justifyContent:"center", fontFamily: "Courier New", display:"flex"}}> {item.login}</div></div>)
             }
         </>
     );
